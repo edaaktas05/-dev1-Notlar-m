9968d9537bda472ed206cacadee92ae69adb4334
Eda Aktas <edam0418@gmail.com>
2023-01-31 22:47:34 +0300

    Odev dosyası oluşturuldu

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*string[] siparişNo = new string[3]; //memory
+
+            siparişNo[0] = "INZ123";
+            siparişNo[1] = "GOOGLE2";
+            siparişNo[2] = "GOOGLE3";
+            siparişNo[0] = "jvfl"; //değişiklik*/
+
+            string[] siparisNo = { "birinciSiparis", "ikinciSiparis", "ucuncuSiparis" };
+            
+            
+            
+            Console.WriteLine(siparisNo.Length);
+            Console.ReadLine();
+        }
+    }
+}
